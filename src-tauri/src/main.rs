4180d8396a3cce4597d12ff477fcaaff12ac5b53
Charlie Carolan <charliecarolan1999@gmail.com>
2023-01-22 00:42:39 +0000

    feat: get prisma client rust working

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,13 +3,19 @@
     windows_subsystem = "windows"
 )]
 
+mod prisma;
+
+use prisma::PrismaClient;
+use prisma_client_rust::NewClientError;
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
-fn main() {
+#[tokio::main]
+async fn main() {
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
